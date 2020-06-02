1f822dc84e4009ed7e411be8ed1e1273cc399839
Szymon R <szym.rajewski@gmail.com>
2020-06-02 07:59:21 +0200

    Add connector, connection class.

---
+++ src/connect/connector.py
@@ -0,0 +1,29 @@
+def listen_devices():
+
+def connect():
+
+def wait_to_establish_connection():
+
+def stop_waiting():
+
+def is_connected():
+
+def retry_to_establish_connection():
+
+def disconnect():
+
+def send_request():
+
+def store_request():
+
+def is_error_occured():
+
+def is_request_delivered_successfully():
+
+def is_request_data_equal():
+
+def get_response():
+
+def set_timeout():
+
+def is_request_timed_out():
